7f4640bbd463d9294d8a75def26779e1f8cb40db
akshay <thesmileynoob@gmail.com>
2016-10-10 17:51:15 +0530

    Shifted view logic to template

--- app/scripts/controllers/attempt.js
+++ app/scripts/controllers/attempt.js
@@ -10,28 +10,21 @@
 angular.module('testingFrontendApp')
   .controller('AttemptCtrl', ['$scope','$state','$sce','attempt','questions','status',
   function($scope,$state,$sce,attempt,questions,status) {
-
-    // Initilization
     $scope.questions = questions.data;
 
-    //Select and display the clicked question
-    $scope.select = function(e, $index){
-      var question_no = $index + 1;
-      displayQuestion($index);
-    }
+    $scope.init = function(questions,status){
+      questions.data.forEach((question) => {
+        status.data.push({id:question.id, answer: question.answer});
+      })
+    };
 
-    // Used with select()
-    function displayQuestion(index){
-      console.log('displaying question:' + (index + 1));
-      var Q = $scope.questions[index];
-      var question_body = Q.question;
-      $scope.selectedQuestion = $sce.trustAsHtml(question_body);
-      $scope.option1 = $sce.trustAsHtml(Q.ans1);
-      $scope.option2 = $sce.trustAsHtml(Q.ans2);
-      $scope.option3 = $sce.trustAsHtml(Q.ans3);
-      $scope.option4 = $sce.trustAsHtml(Q.ans4);
-    }
+    $scope.init(questions, status);
     
+
+    // Set the selected question
+    $scope.select = function(question){
+      $scope.selectedQuestion = question;
+    }
     
   }]);
 
@@ -56,12 +49,12 @@ State: The state of the question - (q*)
   qm: Marked (for later review)  (Depricated. Use qa instead)
 
 Type: Type of the question - (t*)
-  MC: Multiple choice questions
-  SC: Single choice 
-  MT: Matrix Type
-  CH: ???
-  AR: ???
-  SA: ???
+  MC: Multiple choice questions {Checkboxes}
+  SC: Single choice {RadioButtons} 
+  MT: Matrix Type  {???}
+  CH: Comprehension {TextArea}
+  AR: Assertion Reason {???}
+  SA: Subjective Answers {TextArea}
 
 Subject: The subject the question belongs to - (s*)
   math
