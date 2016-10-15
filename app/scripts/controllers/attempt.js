cbb30de27ff98bc90ab04ed55f913cd921096070
akshay <thesmileynoob@gmail.com>
2016-10-15 16:42:33 +0530

    Major Update.
    -Attempt controller documentation bump
    -Form validation bump
    -Window close confirmation box (commented out for dev purposes)
    -Clear button bugfix

--- app/scripts/controllers/attempt.js
+++ app/scripts/controllers/attempt.js
@@ -22,22 +22,27 @@ angular.module('testingFrontendApp')
             })  
         }
       };
+
+      // Set the first question as selected one
+      // TODO instead of setting this as default, Let the first screen
+      // be a mock for INTRO.js
       $scope.setUpQuestions = function(){
         $scope.selectedQuestion = $scope.questions[0];
-        // Create user ans field if not there
-      }
-      $scope.init(questions, status);
-      // Set the selected question
-      $scope.selectQuestion = function(question){
-        // Special case for CH type
-        if(true){
-          console.log(question.ques_type); 
-          // Set the next n questions as assertion type
-        } else {
-        $scope.selectedQuestion = question;
-        }
       }
 
+      $scope.init(questions, status);
+
+      // Set the selected question. Used by the left_panel index
+      $scope.selectQuestion = function(question){
+        if(true){
+          // Special case for CH type
+          console.log(question.ques_type); 
+          // Set the next n questions as assertion type
+        }
+        $scope.selectedQuestion = question;
+      }
+
+      // Go to next question. Used by "Next" button.
       $scope.nextQuestion = function(){
         var currentIndex = $scope.questions.indexOf($scope.selectedQuestion); 
         if(currentIndex < $scope.questions.length - 1){
@@ -47,6 +52,8 @@ angular.module('testingFrontendApp')
         }
       }
 
+      // Go to previous question. 
+      // Used by "Previous" control button.
       $scope.previousQuestion = function(){
         var currentIndex = $scope.questions.indexOf($scope.selectedQuestion); 
         if(currentIndex > 0){
@@ -56,19 +63,32 @@ angular.module('testingFrontendApp')
         }
       }
 
+      // Clear user selected/entered answer for the selectedQuestion
+      // Used by the "clear" control button
+      $scope.clearAnswer = function(){
+        $scope.selectedQuestion.answer = undefined;
+        $scope.selectedQuestion.answerA = undefined;
+        $scope.selectedQuestion.answerB = undefined;
+        $scope.selectedQuestion.answerC = undefined;
+        $scope.selectedQuestion.answerD = undefined;
+      }
+
+      // For testing purposes
+      // Used by the "info" control button
       $scope.displayInfo = function(){
         console.log($scope.selectedQuestion); 
       }
-      $scope.validateAndFormatAnswer = function(question){ //Saves answer if valid, else returns error sting
+
+      // Set question.useranswer.answer if valid, else returns error string for alert
+      // TODO No need to expose this to $scope!! 
+      $scope.validateAndFormatAnswer = function(question){ 
         if (question.ques_type=='SC'){
           var validAnswers = ["a","b","c","d"];
-          
           if (question.answer==undefined || !validAnswers.includes(question.answer)){
             return 'Please select atleast one valid option';
           } else {
             question.useranswer.answer = question.answer;
           }
-
         } else if (question.ques_type=='MC'){
           var ansList = [];
           var ansStr = "";
@@ -78,32 +98,34 @@ angular.module('testingFrontendApp')
           {
             validAnswers.forEach((va) => {
               if(question[va] == true) ansList.push(va[6].toLowerCase()); //Push the last character A/B/C/D
-                ansStr = ansList.toString();  // CSV String 
-                question.useranswer.answer = ansStr;
+              ansStr = ansList.toString();  // CSV String 
+              question.useranswer.answer = ansStr;
             });
           } else {
             return 'Please select atleast one valid option'; 
           }
-          
 
         } else if (question.ques_type=='MT'){
+          // TODO
 
         } else if (question.ques_type=='IT'){
           if(question.answer == undefined) return 'Please enter an Integer';
           else question.useranswer.answer = question.answer;
 
         } else if (question.ques_type=='TF'){
+          // TODO
 
         } else if (question.ques_type=='SA'){
           if(question.answer == undefined) return 'Please write SOMETHING! Jeeze...';
           else question.useranswer.answer = question.answer;
-
-
         }
         return true;
       }
 
+      // Post answer to the backend if valid, else throw an alert. 
+      // Used by the "save" control button.
       $scope.save = function(question){
+        // Instantiate useranswer field if not present. Used to track previous attempts(?)
         if(question.useranswer == undefined){
           question.useranswer = {
             attempt: attempt.attempt.id,
@@ -114,14 +136,15 @@ angular.module('testingFrontendApp')
           console.log('created ans: ');
           console.log( question.useranswer);
         }
-        var ques_valid = $scope.validateAndFormatAnswer(question); // Returns true or error string... ?!
+        var ques_valid = $scope.validateAndFormatAnswer(question); 
 
         if (ques_valid==true){
           useranswer.saveAnswer(question.useranswer)
             .then(function(resp){
               question.useranswer = resp;
             },function(err){
-
+              // TODO
+              console.log("Couldn't save your answer! Please check your internet connection.");
             });
         } else {
           console.log(ques_valid);
@@ -157,9 +180,4 @@ angular.module('testingFrontendApp')
    CH: Comprehension {TextArea}
    AR: Assertion Reason {???}
    SA: Subjective Answers {TextArea}
-
-   Subject: The subject the question belongs to - (s*)
-   math
-
-
 */
