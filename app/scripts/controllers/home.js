095e126f2940b1aa517182a361f4b62e81f34c48
akshay <thesmileynoob@gmail.com>
2016-10-26 18:09:20 +0530

    timezone function stubs

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -8,14 +8,14 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-  .controller('HomeCtrl', ['$scope','$state','available_papers','user_attempts','$uibModal',
-  	function ($scope,$state,available_papers,user_attempts,$uibModal) {
-    	$scope.available_papers = available_papers;
+.controller('HomeCtrl', ['$scope','$state','available_papers','user_attempts','$uibModal',
+    function ($scope,$state,available_papers,user_attempts,$uibModal) {
+      $scope.available_papers = available_papers;
       $scope.user_attempts = user_attempts.data;
       $scope.init = function () {
         /*
-        Does initial book keeping for the attempted papers
-         */
+           Does initial book keeping for the attempted papers
+           */
         for (var i=0; i<$scope.user_attempts.length; i++){
           var p = _.find($scope.available_papers,function(a){return a.id==$scope.user_attempts[i].paper_info.id});
           if ($scope.user_attempts[i].finished!=true) {
@@ -25,6 +25,24 @@ angular.module('testingFrontendApp')
           }
         }
       }
+      $scope.getTzTime = function(time_str){
+        var d = new Date();
+        var offset = d.getTimezoneOffset();
+        var reg = /(.*)T(.*)Z/;
+        var date = reg.exec(time_str)[1]
+        var UTCtime = reg.exec(time_str)[2]
+        var uHH = UTCtime.split(':')[0]
+        var uMM = UTCtime.split(':')[1]
+        var oHH = Math.floor(offset/60);
+        var oMM = offset % 60;
+        var lHH = parseInt(uHH) + parseInt(oHH);
+        var lMM = parseInt(uMM) + parseInt(oMM);
+        var time = (lHH) + ':' + (lMM);
+        console.log(time);
+        
+        return date + " " + time
+      }
+
       $scope.attemptPaper = function(paper){
         var paperInstance = $uibModal.open({
           templateUrl:'views/paper-start.html',
@@ -34,12 +52,12 @@ angular.module('testingFrontendApp')
               $uibModalInstance.close();
               //create the attempt
               attempt.startAttempt(paper.id)
-              .then(function(resp){
-                attempt.setAttempt(resp.data);
-                $state.go('home.attempt',{'pid':resp.data.id,'paper':$scope.paper});  
-              },function(err){
-                //display error
-              });
+                .then(function(resp){
+                  attempt.setAttempt(resp.data);
+                  $state.go('home.attempt',{'pid':resp.data.id,'paper':$scope.paper});  
+                },function(err){
+                  //display error
+                });
             }
           }],
           resolve:{
@@ -48,4 +66,4 @@ angular.module('testingFrontendApp')
         });
       };
       $scope.init();
-  }]);
+    }]);
