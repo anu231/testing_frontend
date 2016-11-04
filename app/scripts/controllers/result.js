4c5e5a1f39df2b3a60eccd600a2091b33b010a2c
akshay <thesmileynoob@gmail.com>
2016-11-04 16:56:40 +0530

    Go back to home.html functionality

--- app/scripts/controllers/result.js
+++ app/scripts/controllers/result.js
@@ -8,7 +8,7 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-.controller('ResultCtrl',['$scope','attempt','result','paper','current_attempt_result','user_attempts','current_paper',function ($scope,attempt,result,paper,current_attempt_result,user_attempts,current_paper) {
+.controller('ResultCtrl',['$scope','$state','attempt','result','paper','current_attempt_result','user_attempts','current_paper',function ($scope,$state,attempt,result,paper,current_attempt_result,user_attempts,current_paper) {
   $scope.current_attempt_result = current_attempt_result.data;
   $scope.user_attempts = user_attempts.data;
   $scope.current_attempt = _.find(user_attempts.data, function(a){return a.id == $scope.current_attempt_result.attempt});
@@ -17,6 +17,11 @@ angular.module('testingFrontendApp')
   this.initialize = function(){
     $scope.result = current_attempt_result.data;
     $scope.selectCurrentPaper;
+  }
+
+  // Data collection and go back
+  $scope.goBack = function(){
+    $state.go('home');
   }
 
   $scope.selectCurrentPaper = function(pid){
@@ -35,9 +40,6 @@ angular.module('testingFrontendApp')
   $scope.getResultForAttemptId = function(aid){
   }
 
-  $scope.goBack = function(){
-    // Go back to home.html 
-  }
 
   //TODO mark calculation functions.
   //TODO chartjs helper functions
