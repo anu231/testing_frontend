6963dbd273edd40f2a42a07068b47537e657ebe2
akshay <thesmileynoob@gmail.com>
2016-11-04 13:55:54 +0530

    Added new service: "result"

---
+++ app/scripts/services/result.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name testingFrontendApp.result
+ * @description
+ * # result
+ * Service in the testingFrontendApp.
+ */
+angular.module('testingFrontendApp')
+  .service('result', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
