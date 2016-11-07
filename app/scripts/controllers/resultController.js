aac3bb15ac4e3c80bf8eae81b0c9faf1de178056
akshay <thesmileynoob@gmail.com>
2016-11-07 15:21:29 +0530

    Basic chart template up and running

--- app/scripts/controllers/resultController.js
+++ app/scripts/controllers/resultController.js
@@ -42,7 +42,13 @@ function ($scope,$state,attempt,result,paper,p_current_attempt_result,p_user_att
   }
 
   //TODO chartjs helper functions
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
 
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
   this.initialize();
 
 }]);
