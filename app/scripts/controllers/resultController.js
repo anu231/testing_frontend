0f7ae98b083028230e2f7801063343646174b78e
akshay <thesmileynoob@gmail.com>
2016-11-07 14:49:25 +0530

    Basic functionality up and running

--- app/scripts/controllers/resultController.js
+++ app/scripts/controllers/resultController.js
@@ -8,40 +8,39 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-.controller('ResultCtrl',['$scope','$state','attempt','result','paper','current_attempt_result','user_attempts','current_paper',function ($scope,$state,attempt,result,paper,current_attempt_result,user_attempts,current_paper) {
-  $scope.current_attempt_result = current_attempt_result.data;
-  $scope.user_attempts = user_attempts.data;
-  $scope.current_attempt = _.find(user_attempts.data, function(a){return a.id == $scope.current_attempt_result.attempt});
-  $scope.current_paper = current_paper;
+.controller('ResultCtrl',['$scope','$state','attempt','result','paper','p_current_attempt_result','p_user_attempts','p_current_paper',
+function ($scope,$state,attempt,result,paper,p_current_attempt_result,p_user_attempts,p_current_paper) {
+  var current_attempt_result = p_current_attempt_result.data;
+  var current_attempt = _.find(p_user_attempts.data, function(a){return a.id == current_attempt_result.attempt});
+  $scope.user_attempts = p_user_attempts.data;
+  $scope.selectedPaper = p_current_paper;
 
   this.initialize = function(){
-    $scope.result = current_attempt_result.data;
-    $scope.selectCurrentPaper();
-  }
+    $scope.selectAttempt();
+  };
 
   // Data collection and go back
   $scope.goBack = function(){
     $state.go('home');
-  }
+  };
 
-  $scope.selectCurrentPaper = function(pid){
-    // selects a paper to display results 
-    // attempt.getResultForAttemptId(pid)
-    console.log($scope.result);
-    console.log($scope.current_attempt);
-    $scope.rank = $scope.result.mrank;
-    $scope.marks_obtained = $scope.result.marksobt;
-    $scope.paper_name = $scope.current_attempt.paper_info.name;
-    var ad = new Date($scope.current_attempt.endtime);
-    $scope.attempt_date = ad.toLocaleDateString();
-    $scope.duration = $scope.current_attempt.paper_info.duration;
+  // Paper selection pipeline
+  $scope.selectAttempt = function(pid){
+    $scope.selectedAttempt = {};
+    $scope.selectedAttempt.rank = current_attempt_result.mrank;
+    $scope.selectedAttempt.marks_obtained = current_attempt_result.marksobt;
+    $scope.selectedAttempt.paper_name = current_attempt.paper_info.name;
+    $scope.selectedAttempt.attempt_date = (function(){
+      var ad = new Date(current_attempt.endtime);
+      return ad.toLocaleDateString();
+    })();
+    $scope.selectedAttempt.duration = current_attempt.paper_info.duration;
+    $scope.selectedAttempt.result = current_attempt_result;
   }
 
   $scope.getResultForAttemptId = function(aid){
   }
 
-
-  //TODO mark calculation functions.
   //TODO chartjs helper functions
 
   this.initialize();
