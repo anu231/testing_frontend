5e95a32376fcfc05127431da04839349cd9cbb11
akshay <thesmileynoob@gmail.com>
2016-11-08 17:59:38 +0530

    Ultra uber major overwhelming update
    Charts working [beta]
    Added more detail to results page

--- app/scripts/controllers/resultController.js
+++ app/scripts/controllers/resultController.js
@@ -13,6 +13,7 @@ function ($scope,$state,attempt,result,paper,p_current_attempt_result,p_user_att
   var current_attempt_result = p_current_attempt_result.data;
   var current_attempt = _.find(p_user_attempts.data, function(a){return a.id == current_attempt_result.attempt});
   $scope.current_paper_attempts = _.filter(p_user_attempts.data, function(a){return a.paper_info.id == current_attempt.paper_info.id});
+  $scope.processed_current_paper_attempts = [];  // One time processing for "Overtime trend" bar chart
   $scope.selectedPaper = p_current_paper;
   // Fetch the paper (Review)
   paper.getPaper(current_attempt.paper_info.id).then(function(resp){
@@ -20,14 +21,17 @@ function ($scope,$state,attempt,result,paper,p_current_attempt_result,p_user_att
     $scope.initialize();
   }, function(err){console.log(err);});
 
- 
 
-  $scope.initialize = function(){
-    $scope.getAllAttemptResults();
-  };
-  $scope.getAllAttemptResults = function () {
+  $scope.initialize = function () {
     result.getAllAttemptResults().then(function (resp) {
       $scope.allAttemptResults = _.filter(resp.data, function (r) { return true });
+      $scope.current_paper_attempts.forEach(function (attempt) {
+        var result = _.find($scope.allAttemptResults, function (result) { return result.attempt == attempt.id });
+        if (result) {
+          attempt.result = result;
+          $scope.processed_current_paper_attempts.push(attempt);
+        }
+      })
       $scope.selectAttempt(current_attempt.id);
     }, function (err) {
       console.log("Couldnt fetch results...");
@@ -39,6 +43,8 @@ function ($scope,$state,attempt,result,paper,p_current_attempt_result,p_user_att
   };
 
   // Paper selection pipeline
+  // Basically sets the $scope.selectedAttempt variable.
+  // $scope.selectedAttempt is used as the model for result area.
   $scope.selectAttempt = function(aid){
     var att = _.find($scope.current_paper_attempts, function(a){return a.id == aid});
     var result = _.find($scope.allAttemptResults, function(r){return att.id == r.attempt})
@@ -68,28 +74,63 @@ function ($scope,$state,attempt,result,paper,p_current_attempt_result,p_user_att
     $scope.selectedAttempt.result = (function(){
       return _.find($scope.allAttemptResults, function(r){return r.attempt == att.id})
     })();
-
-    // Update charts
     $scope.updateCharts();
   }
 
-  $scope.getResultForAttemptId = function(aid){
-  }
-
-  
   //                              >> CHARTS AND ANALYSIS <<                     //
   // Updates charts based on selectedAttempt
   $scope.updateCharts = function () {
     console.log("updating charts");
-    $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-    $scope.series = ['Series A', 'Series B'];
-
-    $scope.data = [
-      [65, 59, 80, 81, 56, 55, 40],
-      [28, 48, 40, 19, 86, 27, 90]
-    ];
+    $scope.updateBarChart();
+    $scope.updatePieChart();
   }
-  // $scope.initialize();
+  
+  $scope.updatePieChart = function(){
+    var r = $scope.selectedAttempt.result;
+    $scope.pie = {};
+    $scope.pie.labels = ["Physics", "Chemistry", "Maths", "Biology", "Zoology"];
+    $scope.pie.data = [r.pobt,r.cobt,r.mobt,r.bobt,r.zobt];
+    $scope.pie.options = {
+      legend:{
+        display: true,
+        position: "right"
+      }
+    };
+  }
+
+  $scope.updateBarChart = function(){
+    var ptrend = [],
+        ctrend = [],
+        mtrend = [],
+        btrend = [],
+        ztrend = []
+
+    // TODO HIDE IF ONLY ONE ATTEMPT
+    // Logic
+    $scope.bar = {};
+    $scope.bar.series = ["Physics", "Chemistry", "Maths", "Biology", "Zoology"];
+    $scope.bar.labels = [];
+    $scope.processed_current_paper_attempts.forEach(function(a){
+      var date = new Date(a.endtime);
+      $scope.bar.labels.push(date.toLocaleDateString());
+      ptrend.push(a.result.pobt)
+      ctrend.push(a.result.cobt)
+      mtrend.push(a.result.mobt)
+      btrend.push(a.result.bobt)
+      ztrend.push(a.result.zobt)
+    });
+
+    // Graphics
+
+    $scope.bar.data = [ptrend, ctrend, mtrend, btrend, ztrend];
+    $("#leg").html = 
+    $scope.bar.options = {
+      legend: {
+            display: true,
+        }
+    }
+  }
+
 
   $scope.debug = function(){
     console.log($scope);
