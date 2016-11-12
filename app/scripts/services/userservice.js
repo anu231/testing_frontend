714cdcbed8d1e7106df79f7f3bc300b0aa14a85d
akshay <thesmileynoob@gmail.com>
2016-11-12 15:18:06 +0530

    User service
    For getting user profile, logouts etc.

---
+++ app/scripts/services/userservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name testingFrontendApp.userService
+ * @description
+ * # userService
+ * Service in the testingFrontendApp.
+ */
+angular.module('testingFrontendApp')
+  .service('userService', function () {
+    
+  });
