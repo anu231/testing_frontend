e5ded16ff573c9ac7c2a8a0519edf8e3538a267b
akshay <thesmileynoob@gmail.com>
2016-11-16 16:45:31 +0530

    added basic fuctionality

--- app/scripts/controllers/solutionsController.js
+++ app/scripts/controllers/solutionsController.js
@@ -8,6 +8,14 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-  .controller('SolutionsCtrl', function () {
-    
-  });
+  .controller('SolutionsCtrl', ['$scope', '$state', 'solutions',
+    function ($scope, $state, solutions) {
+      console.log(solutions.data);
+      $scope.solutions = solutions.data;
+      $scope.questions = solutions.data;
+      console.log($scope.questions);
+  
+
+
+    }
+  ]);
