97e79b8c8d6f6764a65a5e31db64d7aace197480
akshay <thesmileynoob@gmail.com>
2016-11-22 15:37:12 +0530

    Fixed dependencies

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -8,8 +8,8 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-  .controller('MainCtrl',['$state','$rootScope','$scope', 'userService', 
-  	function ($state,$rootScope,$scope, userService) {
+  .controller('MainCtrl',['$state','$rootScope','$scope', 'userService', '$timeout', '$window',
+  	function ($state,$rootScope,$scope, userService, $timeout, $window) {
     $scope.init = function(){
     	//$rootScope.baseURL = 'http://localhost:8000/';
     	//$scope.uirouterDebug();
@@ -19,7 +19,7 @@ angular.module('testingFrontendApp')
 			}, function(err){
 				console.log(err);
 				alert("Error You are not logged in / authorized! Please log in to continue");
-        $timeout(function(){$window.location.href="http://www.raoeduconnect.com"}, 3000);
+        // $timeout(function(){$window.location.href="http://www.raoeduconnect.com"}, 3000);
 			});
 			$scope.logout = function(){
 				userService.logout();
