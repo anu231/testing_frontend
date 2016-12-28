675f39f65f9da4dd9dcc7cb8ffd9ace9d0cff672
akshay <thesmileynoob@gmail.com>
2016-12-28 16:27:39 +0530

    Fixed back button

--- app/scripts/controllers/solutionsController.js
+++ app/scripts/controllers/solutionsController.js
@@ -11,18 +11,21 @@ angular.module('testingFrontendApp')
   .controller('SolutionsCtrl', ['$scope', '$state', 'solutions', 'attempt',
     function ($scope, $state, solutions, attempt) {
       document.title = "Solutions and Answer Key";
-      console.log(attempt);
+      $('#loading_papers').hide();
       $scope.attempt = attempt;
       $scope.solutions = solutions.data;
 
       $scope.questions = $scope.solutions
       console.log($scope.questions);
 
-      $scope.showSolution = function(question){
-        question.show_solution ? question.show_solution = false : question.show_solution=true;
+      $scope.showSolution = function (question) {
+        question.show_solution ? question.show_solution = false : question.show_solution = true;
         console.log(question);
       }
 
+      $scope.goBack = function () {
+        window.history.back();
+      };
 
     }
   ]);
