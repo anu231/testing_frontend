c54b1c6cfca2aca01e5758ea7ddcdfa8fc30498a
akshay <thesmileynoob@gmail.com>
2017-01-02 17:09:00 +0530

    Removed image sanitization

--- app/scripts/controllers/solutionsController.js
+++ app/scripts/controllers/solutionsController.js
@@ -8,15 +8,18 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-  .controller('SolutionsCtrl', ['$scope', '$state', 'solutions', 'attempt',
-    function ($scope, $state, solutions, attempt) {
+  .controller('SolutionsCtrl', ['$scope', '$state', 'solutions', 'attempt', '$sce',
+    function ($scope, $state, solutions, attempt, $sce) {
       document.title = "Solutions and Answer Key";
       $('#loading_papers').hide();
       $scope.attempt = attempt;
       $scope.solutions = solutions.data;
-
       $scope.questions = $scope.solutions
-      console.log($scope.questions);
+
+      // Don't sanatize Latex images
+      $scope.trustedHtml = function (html) {
+        return $sce.trustAsHtml(html);
+      }
 
       $scope.showSolution = function (question) {
         question.show_solution ? question.show_solution = false : question.show_solution = true;
