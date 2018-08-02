2f5958d7cc9ab7e9657f7ec753fe02c9b14d1046
Abhishek <abhishek.pawar@raoiit.com>
2018-08-02 12:13:25 +0530

    pre pull commit

--- app/scripts/controllers/paper.js
+++ app/scripts/controllers/paper.js
@@ -8,10 +8,15 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-  .controller('PaperCtrl', ['$scope','$http','paper_obj','attempts', function ($scope,$http,paper_obj, attempts) {
+  .controller('PaperCtrl', ['$scope','$http','paper_obj','attempts','result', function ($scope,$http,paper_obj, attempts, result) {
       $scope.init = function(){
         console.log(paper_obj);
+        console.log(attempts.data);
+        $scope.paper_obj = paper_obj;
+        $scope.attempts = attempts.data;
+
         $('#loading_papers').hide();
       }
+      
       $scope.init();
 }]);
