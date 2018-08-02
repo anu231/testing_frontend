de319cf2739807149dee8cf4178a4283931f96a8
Anurag <anu231@gmail.com>
2018-08-02 12:11:03 +0530

    function added for getting attempt data

--- app/scripts/controllers/paper.js
+++ app/scripts/controllers/paper.js
@@ -8,10 +8,18 @@
  * Controller of the testingFrontendApp
  */
 angular.module('testingFrontendApp')
-  .controller('PaperCtrl', ['$scope','$http','paper_obj','attempts', function ($scope,$http,paper_obj, attempts) {
+  .controller('PaperCtrl', ['$scope','$http','paper_obj','attempts','result', function ($scope,$http,paper_obj, attempts, result) {
       $scope.init = function(){
         console.log(paper_obj);
         $('#loading_papers').hide();
-      }
+      };
+      $scope.get_attempt_result = function(attempt_id){
+        result.getAttemptResult(attempt_id)
+        .then(function(resp){
+            $scope.attempt_result = resp.data;
+        },function(err){
+            //show notification
+        });
+      };
       $scope.init();
 }]);
